eb335a73d759eab6f3fb514c6413bdc45d02d620
MaurizioVacca <mauriziovacca@easy-network.it>
2019-06-26 10:35:11 +0200

    Add options parameter definition

--- src/connectForm.js
+++ src/connectForm.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 import { Provider } from './context';
 
-const connectForm = (Target) => {
+/**
+ * Connect the form properties, such schema, validation mode, etc. to the `Component`
+ * designed as wrapper.
+ *
+ * @param {Object} [options] - the configuration options.
+ * @param {Object} [options.schema] - the form schema.
+ *  Schema is used to define defaults form values, the initial state and the validation rules.
+ * @param {string} [options.schemaVendor='yup'] - the validation schema vendor.
+ * @param {string} [options.validationMode='onsubmit'] - the validation mode used.
+ *  Allowed values are: `onsubmit`, `onblur`, `onchange`
+ * @param {boolean} [options.statePropagation=false] - whether the stage changes should be
+ *  propagated or not.
+ *
+ * @return {function(*): function(*): *} -
+ *  The `Form` component enhanced with listeners and properties.
+ */
+const connectForm = ({
+  schema,
+  schemaVendor = 'yup',
+  validationMode = 'onsubmit',
+  statePropagation = false,
+} = null) => (Target) => {
   function Form(props) {
     const [values, setValues] = useState({});
 
