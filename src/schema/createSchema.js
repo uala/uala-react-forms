3663ca66cf7c587ac2d4296e0628d1ca6359f1ac
MaurizioVacca <mauriziovacca@easy-network.it>
2019-06-27 14:07:09 +0200

    Improve jsdoc

--- src/schema/createSchema.js
+++ src/schema/createSchema.js
@@ -4,8 +4,10 @@ import createFromYupSchema from './createFromYupSchema';
 import createFromCustomSchema from './createFromCustomSchema';
 
 /**
+ * Create a convenient schema wrapper to access validate and defaults methods.
  *
- * @param {any} schema
+ * @param {Object|null} schema - the schema model
+ * @param {string} vendor - the vendor name. E.g. yup
  * @returns {UalaSchemaWrapper} - the schema wrapper
  */
 const createSchema = (schema, vendor) => {
