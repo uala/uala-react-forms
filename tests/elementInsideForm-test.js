c3b1beac8a98ecd48075eb5e3e0e1bade595510a
Michele Salvini <info@garagecode.it>
2019-06-27 10:33:34 +0200

    added async on onChange test

--- tests/elementInsideForm-test.js
+++ tests/elementInsideForm-test.js
@@ -16,30 +16,27 @@ const Form = connectForm({ schema })(({ children }) => <div>{children}</div>);
 const TextInput = connectFormElement(({ name, onChange }) => (
   <input name={name} onChange={e => onChange(name, e.target.value)} />
 ));
-const DisplayFirstName = connectFormElement(({ values }) => (
-  <div dataValues={values}>{values.first_name}</div>
-));
+const DisplayFirstName = connectFormElement(({ values }) => <div dataValues={values}>{values.first_name}</div>);
 
 describe('Form with elements', () => {
-  it('Mount', (done) => {
+  it('Mount', async () => {
     let tree;
-    const renderedForm = create(<Form>
+    const renderedForm = create(
+      <Form>
         <TextInput name="first_name" />
         <TextInput name="last_name" />
         <DisplayFirstName />
-      </Form>,);
+      </Form>
+    );
     tree = renderedForm.toJSON();
 
     expect(tree.children.length).toBe(3);
     expect(tree.children[0].props.name).toBe('first_name');
     expect(tree.children[0].props.onChange).toBeA('function');
-    tree.children[0].props.onChange({ target: { value: 'Mark' } });
+    await tree.children[0].props.onChange({ target: { value: 'Mark' } });
 
     tree = renderedForm.toJSON();
     expect(tree.children[2].children[0]).toBe('Mark');
     expect(tree.children[2].props.dataValues.first_name).toBe('Mark');
-    setTimeout(() => {
-      done();
-    }, 10);
   });
 });
