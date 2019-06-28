396822fe4704f056646fb8dc5a58993419f9efd7
MaurizioVacca <mauriziovacca@easy-network.it>
2019-06-28 13:15:30 +0200

    Add errors display demo

--- demo/src/TextInput.js
+++ demo/src/TextInput.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { connectFormElement } from '../../src';
 
+const errorHelper = {
+  color: 'red',
+};
+
 // eslint-disable-next-line react/prop-types
-const TextInput = ({ name, onChange, defaultValue }) => (
-  <div className="TextInput">
-    <input type="text" defaultValue={defaultValue} name={name} onChange={e => onChange(name, e.target.value)} />
-  </div>
-);
+const TextInput = ({ name, errors, onChange, defaultValue }) => {
+  return (
+    <div className="TextInput">
+      <input type="text" defaultValue={defaultValue} name={name} onChange={e => onChange(name, e.target.value)} />
+      {errors && <div style={{ ...errorHelper }}>{errors}</div>}
+    </div>
+  );
+};
 
 export default connectFormElement(TextInput);
--- demo/src/schema.js
+++ demo/src/schema.js
@@ -3,6 +3,7 @@ import { object, string } from 'yup';
 const schema = object({
   first_name: string()
     .required('This field is required')
+    .min(4)
     .default('pippo'),
 });
 
