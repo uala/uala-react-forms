04a2e4c757c0264e275d30a84dc1eb5259364d06
MaurizioVacca <mauriziovacca@easy-network.it>
2019-06-28 16:39:17 +0200

    Update demo

--- demo/src/TextInput.js
+++ demo/src/TextInput.js
@@ -6,11 +6,11 @@ const errorHelper = {
 };
 
 // eslint-disable-next-line react/prop-types
-const TextInput = ({ name, errors, onChange, defaultValue }) => {
+const TextInput = ({ name, errors, onChange, values }) => {
   return (
     <div className="TextInput">
-      <input type="text" defaultValue={defaultValue} name={name} onChange={e => onChange(name, e.target.value)} />
-      {errors && <div style={{ ...errorHelper }}>{errors}</div>}
+      <input type="text" defaultValue={values[name]} name={name} onChange={e => onChange(name, e.target.value)} />
+      {errors && <div style={{ ...errorHelper }}>{errors[name]}</div>}
     </div>
   );
 };
