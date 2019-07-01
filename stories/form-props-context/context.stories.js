6652d244c21b15e9cb12f89a977c7d9939aa3389
Michele Salvini <info@garagecode.it>
2019-07-01 23:23:08 +0200

    dynamic timeout in lazy validation

--- stories/form-props-context/context.stories.js
+++ stories/form-props-context/context.stories.js
@@ -4,11 +4,13 @@ import { storiesOf } from '@storybook/react';
 import Form from './Form';
 import Input from './Input';
 
+const TIMEOUT_S = 3;
+
 const LazyValidation = () => {
   const [formContext, setFormContext] = useState({ min_username_length: 0 });
 
   useEffect(() => {
-    const timeout = setTimeout(() => setFormContext({ min_username_length: 3 }), 4000);
+    const timeout = setTimeout(() => setFormContext({ min_username_length: 3 }), TIMEOUT_S * 1000);
     return () => clearTimeout(timeout);
   }, []);
 
@@ -17,7 +19,7 @@ const LazyValidation = () => {
       <Input name="first_name" label="First name" />
       <Input name="last_name" label="Last name" />
       <Input name="username" label={`Username (min length: ${formContext.min_username_length})`} />
-      <p>Form re-validate automatically after 4 seconds</p>
+      <p>Form re-validate automatically after {TIMEOUT_S} seconds</p>
     </Form>
   );
 };
