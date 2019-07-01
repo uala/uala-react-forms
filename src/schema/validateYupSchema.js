8515302d8bac038942ffc0b439193585c05d2505
MaurizioVacca <mauriziovacca@easy-network.it>
2019-07-01 09:20:16 +0200

    Add jsdoc to validateYupSchema

--- src/schema/validateYupSchema.js
+++ src/schema/validateYupSchema.js
@@ -1,3 +1,4 @@
+import './Schema.type';
 import castInterface from '../castInterface';
 
 import standardiseYupErrors from './standardiseYupErrors';
@@ -5,6 +6,17 @@ import UalaSchemaErrorInterface from './UalaSchemaErrorInterface';
 
 const ALLOWED_ERROR = 'ValidationError';
 
+/**
+ * Validate the form values using a Yup schema. The validation runs asynchronously
+ * and returns a promise.
+ * Once validation ends, the promise will contain the validation result.
+ *
+ * @param {YupSchema} schema - the validation schema
+ * @param {Object} values - the form values
+ * @param {any} [context] - the additional context, if any
+ *
+ * @returns {Promise<any>}
+ */
 const validateYupSchema = (schema, values, context = {}) =>
   new Promise((resolve, reject) => {
     // Yup will throw a `ValidationError` whenever the validation fails
