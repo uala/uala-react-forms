f228ce20c3652c24482d63ca15a39bdf6836f192
MaurizioVacca <mauriziovacca@easy-network.it>
2019-07-01 09:37:29 +0200

    Add jsdoc to getErrorByName

--- src/schema/getErrorByName.js
+++ src/schema/getErrorByName.js
@@ -1,3 +1,11 @@
+/**
+ * Helper to retrieve an error message by name.
+ *
+ * @param errors - the array of errors
+ * @param name - the element name (generally match with form values)
+ * @returns {*|null}
+ */
+
 const getErrorByName = (errors, name) => {
   const needle = errors.find(error => error.name === name);
 
