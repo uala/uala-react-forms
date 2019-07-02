62e6b302f5f0162d783482077d366403f7ec8f96
MaurizioVacca <mauriziovacca@easy-network.it>
2019-07-02 17:09:31 +0200

    Export events definition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import connectFormElement from './connectFormElement';
 import connectForm from './connectForm';
 import { getErrorByName } from './schema';
+import { ON_SUBMIT, ON_DID_CHANGE, ON_CHANGE } from './connectForm.events';
 
-export { connectForm, connectFormElement, getErrorByName };
+export { connectForm, connectFormElement, getErrorByName, ON_SUBMIT, ON_DID_CHANGE, ON_CHANGE };
